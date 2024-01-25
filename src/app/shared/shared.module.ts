a068738cfa9e818ccd58fdd2c3bc278975497d90
m1jlariosm <jululama1@gmail.com>
2024-01-25 09:38:35 -0600

    Fin seccion 9

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContacPageComponent } from './pages/contac-page/contac-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+
+
+@NgModule({
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContacPageComponent,
+    SearchBoxComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContacPageComponent,
+    SearchBoxComponent,
+  ]
+})
+export class SharedModule { }
